e04e9c1f52de7b69b1feaf7f968c78eb86fd8260
only1tej <tijanienny3@gmail.com>
2023-07-23 18:59:21 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import useFetchJobs from "./useFetchJobs";
+import { Container } from "react-bootstrap";
+
+function App() {
+  const { jobs, loading, error } = useFetchJobs();
+  return (
+    <Container>
+      {loading && <h1>Loading....</h1>}
+      {error && <h1>Error, Try Refreshing....</h1>}
+      {<h1>{jobs.length}</h1>}
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/useFetchJobs.js
@@ -0,0 +1,43 @@
+import { useReducer, useEffect } from "react";
+import axios from "axios";
+
+const ACTIONS = {
+  MAKE_REQUEST: "make-request",
+  GET_DATA: "get-data",
+  ERROR: "error",
+};
+
+const BASE_URL = "https://jobs.github.com/positions.json";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.MAKE_REQUEST:
+      return { loading: true, jobs: [] };
+    case ACTIONS.GET_DATA:
+      return { ...state, loading: false, jobs: action.payload.jobs };
+    case ACTIONS.ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload.error,
+        jobs: [],
+      };
+    default:
+      return state;
+  }
+}
+
+export default function useFetchJobs(params, page) {
+  const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true });
+
+  useEffect(() => {
+    dispatch({ type: ACTIONS.MAKE_REQUEST });
+    axios.get(BASE_URL);
+  }, [params, page]);
+
+  return {
+    jobs: [4, 5, 6],
+    loading: false,
+    error: true,
+  };
+}
